f4bf1a4a3c732be5b75fe4765840ddc7581025b4
KuldeepPandya <108853537+KuldeepPandya@users.noreply.github.com>
2023-03-01 00:22:32 +0530

    Add files via upload

---
+++ Assignment/NaturalNumber_3b.java
@@ -0,0 +1,15 @@
+package Assignment;
+
+public class NaturalNumber_3b 
+{
+	public static void main(String[] args)  
+	{
+		int i = -1;
+		
+		while(i<10)
+		{
+			i++;
+			System.out.println(i);
+		}
+	}
+}
